cc061ecc16fdc21f74f31d31e5dca975d442d0eb
SPARRYW <scott.westerbeck@gmail.com>
2021-10-27 15:55:04 -0400

    firstJSX

--- React/firstJSX.js/src/App.js
+++ React/firstJSX.js/src/App.js
@@ -5,21 +5,15 @@ import './App.css';
 function App() {
   return (
 <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      <</header>
-    </div>
-  );
+    <h1>Hello Dojo!</h1><br></br>
+    <h3>Things I need to do:</h3><br></br>
+    <ul>
+      <li>Learn React</li>
+      <li>Climb Mt. Everest</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
+    </ul>
+</div>
+  );}
 
 export default App;
