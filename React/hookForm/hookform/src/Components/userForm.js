4d27ea041a370d9808cfc5e9017158af927b4fd5
SPARRYW <scott.westerbeck@gmail.com>
2021-10-30 22:37:30 -0400

    userForm

--- React/hookForm/hookform/src/Components/userForm.js
+++ React/hookForm/hookform/src/Components/userForm.js
@@ -8,8 +8,48 @@ const UserForm = (props) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
+    const [firstNameError, setfirstNameError] = useState("");
+    const [lastNameError, setlastNameError] = useState("");
+    const [emailError, setEmailError] = useState("")
+    const [passwordError, setPasswordError] = useState("")
     
-    const createUser = (e) => {
+    const verifyFirst_name = (e) => {
+        setfirst_name(e.target.value);
+        if(e.target.value.length < 1){
+            setfirstNameError("First name is required.");
+        } else if(e.target.value.length < 3){
+            setfirstNameError("First name must be 3 characters")
+        }
+    }   
+    const verifyLast_name = (e) => {
+        setlast_name(e.target.value);
+        if(e.target.value.length < 1){
+            setlastNameError("Last name is required.");
+        } else if(e.target.value.length < 3){
+            setlastNameError("Last name must be 3 characters")
+        }
+    }   
+
+    const verifyemail = (e) => {
+        setEmail(e.target.value);
+        if(e.target.value.length < 1){
+            setEmailError("Email is required.");
+        } else if(e.target.value.length < 3){
+            setEmailError("Email must be 3 characters")
+        }
+    }    
+    const verifypassword = (e) => {
+        setPassword(e.target.value);
+        if(e.target.value !== e.target.value)
+            setPasswordError("Passwords must match")
+        if(e.target.value.length < 1){
+            setPasswordError("Password is required.");
+        } else if(e.target.value.length < 8){
+            setPasswordError("Password must be 8 characters")
+        }
+    }    
+
+    const createUser = (e) => {
         e.preventDefault();
         const newUser = { username, first_name, last_name, email, password };
         console.log("Welcome", newUser);
@@ -38,18 +78,38 @@ const UserForm = (props) => {
             <div>
                 <label>First Name: </label> 
                 <input type="text" onChange={ (e) => setfirst_name(e.target.value)} value = {first_name} />
+                {
+                    firstNameError ?
+                    <p style>{{color: 'red'}}>{ firstNameError}</p>:
+                    ''
+                }
             </div>
             <div>
                 <label>Last Name: </label> 
                 <input type="text" onChange={ (e) => setlast_name(e.target.value)} value = {last_name} />
+                {
+                    lastNameError ?
+                    <p style>{{color: 'red'}}>{ lastNameError}</p>:
+                    ''
+                }
             </div>
             <div>
                 <label>Email Address: </label> 
                 <input type="text" onChange={ (e) => setEmail(e.target.value) } value = {email} />
+                {
+                    emailError ?
+                    <p style>{{color: 'red'}}>{ emailError}</p>:
+                    ''
+                }
             </div>
             <div>
                 <label>Password: </label>
                 <input type="text" onChange={ (e) => setPassword(e.target.value) } value = {password} />
+                {
+                    passwordError ?
+                    <p style>{{color: 'red'}}>{ passwordError}</p>:
+                    ''
+                }
             </div>
             <div>
                 <label>Confirm Password: </label> 
