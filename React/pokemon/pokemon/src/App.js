ec1da4b3775a044e46bad57cd043ffb12d2215ef
SPARRYW <scott.westerbeck@gmail.com>
2021-11-03 17:35:16 -0400

    axios pokemon

--- React/pokemon/pokemon/src/App.js
+++ React/pokemon/pokemon/src/App.js
@@ -1,16 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from "react"
+import axios from 'axios';
 
 function App() {
   const [pokemon, setpokemon] = useState([])
 
-  const fetchapi = () => fetch("https://pokeapi.co/api/v2/pokemon")
+  const fetchapi = () => axios.get("https://pokeapi.co/api/v2/pokemon")
             .then(response => {
-                return response.json();
-            }).then(response => {
                 console.log(response);
-                setpokemon(response.results);
+                setpokemon(response.data.results);
             }).catch(err=>{
                 console.log(err);
             })
