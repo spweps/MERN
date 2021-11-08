71c44d17cb75271e23f0ffb8589996ec5a7cb825
SPARRYW <scott.westerbeck@gmail.com>
2021-11-08 16:15:20 -0500

    data

--- Express/Jokes/models/jokes.model.js
+++ Express/Jokes/models/jokes.model.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
  
 const JokeSchema = new mongoose.Schema({
-    setup: String,
-    punchline: String
+    {
+    setup:{
+    type: String,
+    required: [true, "Setup is required"],
+    minlength: [8, "Setup must be at least 8 characters"]
+    },
+    punchline: {
+    type: String,
+    required: [true, "Punchline is required"],
+    minlength: [12, "Punchline must be 12 characters"]
+    }}
 });
- 
+
 const Joke = mongoose.model('Joke', JokeSchema);
- 
+
 module.exports = Joke;
